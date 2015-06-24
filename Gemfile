7dc3ba8d794e95fcf2e0278eff91fc9491c42b7d
Doyi Lee <doyilee@hotmail.com>
2015-06-24 16:32:27 -0600

    fixed bug for submitting category_id

--- Gemfile
+++ Gemfile
@@ -45,5 +45,6 @@ group :development, :test do
   gem 'spring'
   #rspec gem for testing
 	gem 'rspec-rails', '~> 3.0'
+
 end
 
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -48,7 +48,7 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-  	params.require(:item).permit(:name, :price, :description)
+  	params.require(:item).permit(:name, :price, :description, :category_id)
   end
 
   def find_item
