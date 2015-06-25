3c894486cc342eb60d801e9927e291dd7538fdeb
Tiffany Forsyth <tiffanyforsyth1@gmail.com>
2015-06-25 12:06:47 -0600

    added paperclip code to new items form

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ActiveRecord::Base
   validates_presence_of :name
 	belongs_to :category
-	has_attached file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#"}
+	has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#"}
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
