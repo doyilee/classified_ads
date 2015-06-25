f3e35791595f9edab380dfc91c1e35691c81046c
Doyi Lee <doyilee@hotmail.com>
2015-06-25 14:13:43 -0600

    added subcategory id to items model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150625182638) do
+ActiveRecord::Schema.define(version: 20150625201117) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20150625182638) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "subcategory_id"
   end
 
   create_table "subcategories", force: :cascade do |t|
